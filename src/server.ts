49f35e45d52f5009618de306c33932830075cd6c
Miguel Pombo <mp96dev@gmail.com>
2020-12-23 20:23:03 -0300

    Initial Commit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import * as dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.listen(3333, () => {
+  if (process.env.SERVER) {
+    console.log('Dev Server Initalized');
+  }
+});
